aa88d75d102854ff0857102e7245e7d16b793aae
davideserra <46239159+davideserra@users.noreply.github.com>
2025-09-01 20:57:02 +0200

    Update dummy.go
    add func to force export in go

--- bind/dummy.go
+++ bind/dummy.go
@@ -1,4 +1,9 @@
 package bind
 
-// Dummy è un placeholder per rendere il pacchetto non vuoto
-func Dummy() {}
+// Version è un segnaposto per rendere il pacchetto valido
+const Version = "0.0.1"
+
+// Hello è una funzione fittizia solo per avere un export
+func Hello(name string) string {
+    return "Hello " + name
+}
